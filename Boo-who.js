216f332a95bcb0c1cc6c153bc3e2949d9f733284
Bradley Sands <bradley.sands@colorado.edu>
2017-07-03 13:45:50 -0600

    Create Boo-who.js
    Check if a value is classified as a boolean primitive. Return true or false.
    
    Boolean primitives are true and false.
    
    Here are some helpful links:
    
    Boolean Objects
    
    booWho(true) should return true.
    booWho(false) should return true.
    booWho([1, 2, 3]) should return false.
    booWho([].slice) should return false.
    booWho({ "a": 1 }) should return false.
    booWho(1) should return false.
    booWho(NaN) should return false.
    booWho("a") should return false.
    booWho("true") should return false.
    booWho("false") should return false.

---
+++ Boo-who.js
@@ -0,0 +1,13 @@
+
+function booWho(bool) {
+  // What is the new fad diet for ghost developers? The Boolean.
+  var answer;
+  if (bool === true || bool === false) {
+    answer = true;
+  } else {
+    answer = false;
+  }
+  return answer;
+}
+
+booWho(null);
